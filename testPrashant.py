9bbbd74b12c5b3a3391ffc1402689b65830dc150
jOh <j0h@example.com>
2020-05-10 12:00:20 +0200

    Enjoy

--- testPrashant.py
+++ testPrashant.py
@@ -2,8 +2,8 @@
 
 hello = input("How are You?")
 if hello =="Yes":
-    print("Good Morning"
-    print("Enjoy ur day...")
-    print("Hello")
+    print("Good Morning")
+    print("Have a Good Day")
+
 else:
     print("Take Care of Yourself")
